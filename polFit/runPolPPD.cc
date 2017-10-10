ec392b4c3898b8e6e7effdf748ba54ef8921bf82
thomas madlener <thomas.madlener@cern.ch>
2017-10-10 15:15:15 +0200

    Add makefile and wrapper around polPPD.C to have easier access from cl.

---
+++ polFit/runPolPPD.cc
@@ -0,0 +1,22 @@
+#include "general/ArgParser.h"
+
+#include "polPPD.C"
+
+#include <string>
+
+#if !(defined(__CINT__) or defined(__CLING__))
+int main(int argc, char *argv[])
+{
+  ArgParser parser(argc, argv);
+  const auto sigfile = parser.getOptionVal<std::string>("--sigfile");
+  const auto reffile = parser.getOptionVal<std::string>("--reffile");
+  const auto sigtree = parser.getOptionVal<std::string>("--sigtree");
+  const auto reftree = parser.getOptionVal<std::string>("--reftree");
+  const auto outfile = parser.getOptionVal<std::string>("--outfile", "polPPD.root");
+
+  polPPD(sigfile, reffile, sigtree, reftree, outfile);
+
+  return 0;
+}
+
+#endif
