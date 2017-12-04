b062e5b6cee0487856e189e0973c8dc8dd9ae7f4
thomas madlener <thomas.madlener@cern.ch>
2017-12-04 15:20:33 +0100

    Add possibility to run only on one set of input files. Adapt to fixNorm parameter

--- polFit/runLamRefScan.py
+++ polFit/runLamRefScan.py
@@ -12,16 +12,20 @@ from utils.plotHelpers import mkplot
 from utils.TGraph_utils import createGraphSym
 # import json
 
-def run_scan(datafn, reffn, ref_range=[-0.3, 1], n_points=250):
+def run_scan(datafn, reffn, ref_range=[-0.3, 1], n_points=250, output_fn=None):
     """
     Run a scan using n_points in ref_range
+
+    COULDDO: go to a direct call of do_fit and do some of the setup work here,
+    to avoid recreating histograms here for each scan_point
     """
     ref_scan_points = np.linspace(ref_range[0], ref_range[1], n_points)
     fit_results = []
     for lth_ref in ref_scan_points:
-        fit_results.append(run(datafn, reffn, 'foo', 'genData',
+        fit_results.append(run(datafn, reffn, output_fn, 'chic_tuple',
                                chic1_limits=False, fix_ref=lth_ref,
-                               fit_range=False, save=False))
+                               fit_range=False, save=output_fn is not None,
+                               fix_norm=False))
 
     return fit_results
 
@@ -35,7 +39,7 @@ def make_graphs(lth_ref, delta_lth, delta_lth_err, chi2):
     return delta_graph, chi2_graph
 
 
-def eval_fits(fit_results, datafn, plot=False, outdir='.'):
+def eval_fits(fit_results, datafn='', plot=False, outdir='.'):
     """Evaluate the fit results"""
     lth_ref = np.array([r['lth_ref'][0] for r in fit_results])
     delta_lth = np.array([r['delta_lth'][0] for r in fit_results])
@@ -43,13 +47,18 @@ def eval_fits(fit_results, datafn, plot=False, outdir='.'):
     chi2 = np.array([r['chi2'] for r in fit_results])
 
     min_chi2_idx = np.argmin(chi2)
-    lth_data_in = read_gen_config(datafn)['lthsig']
+    if datafn:
+        lth_data_in = read_gen_config(datafn)['lthsig']
+    else:
+        lth_data_in = 0
 
     if plot:
-        delta_graph, chi2_graph = make_graphs(lth_ref, delta_lth + lth_ref, delta_lth_err, chi2)
+        delta_graph, chi2_graph = make_graphs(lth_ref, delta_lth, delta_lth_err, chi2)
         condMkDir(outdir)
-        mkplot(delta_graph, saveAs='/'.join([outdir, 'delta_lth_v_lth_ref.pdf']), drawOpt='PE')
-        mkplot(chi2_graph, saveAs='/'.join([outdir, 'chi2_v_lth_ref.pdf']), drawOpt='PE')
+        mkplot(delta_graph, saveAs='/'.join([outdir, 'delta_lth_v_lth_ref.pdf']), drawOpt='PE',
+               xLabel='#lambda_{ref}', yLabel='#Delta_{#lambda}')
+        mkplot(chi2_graph, saveAs='/'.join([outdir, 'chi2_v_lth_ref.pdf']), drawOpt='PE',
+               xLabel='#lambda_{ref}', yLabel='#chi^{2}')
 
         return [lth_ref[min_chi2_idx], delta_lth[min_chi2_idx],
                 delta_lth_err[min_chi2_idx], lth_data_in, np.min(chi2)]
@@ -71,13 +80,22 @@ def run_all_gens(gendir, outdir):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script for running a scan of different'
                                      ' (fixed) lambda reference values over the ToyMC data')
-    # parser.add_argument('dataFileName', help='filename of the data tuple')
-    # parser.add_argument('refFileName', help='filename of the reference tuple')
-    parser.add_argument('gendir', help='gen data directory')
-    parser.add_argument('-o', '--outdir', help='output directory name', default='fit_results')
+    parser.add_argument('-d', '--dataFileName', help='filename of the data tuple')
+    parser.add_argument('-r', '--refFileName', help='filename of the reference tuple')
+    parser.add_argument('-g', '--gendir', help='gen data directory', type=str, default='')
+    parser.add_argument('-o', '--outdir', help='output directory name (or output file name'
+                        ' when a data and a reference file are passed)',
+                        default='fit_results')
 
     args = parser.parse_args()
 
     gROOT.SetBatch()
 
-    run_all_gens(args.gendir, args.outdir)
+    if args.gendir:
+        run_all_gens(args.gendir, args.outdir)
+    if args.dataFileName and args.refFileName:
+        fit_res = run_scan(args.dataFileName, args.refFileName, output_fn=args.outdir)
+        eval_fits(fit_res, datafn='', plot=True, outdir='.')
+        df = pd.DataFrame(fit_res)
+
+        df.to_pickle(args.outdir.replace('.root', '.pkl'))
